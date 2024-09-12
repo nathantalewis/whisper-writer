8827d018971fb222126889ba6182417f68968b26
Dariox <dariox@privacyrequired.com>
2024-09-12 10:59:41 +0000

    Upgrade to Python 3.12

--- run.py
+++ run.py
@@ -1,8 +1,31 @@
 import os
 import sys
 import subprocess
-from dotenv import load_dotenv
 
-print('Starting WhisperWriter...')
-load_dotenv()
+
+def load_env(env_file='.env'):
+    if os.path.exists(env_file):
+        with open(env_file, 'r') as file:
+            for line in file:
+                line = line.strip()
+                if line and not line.startswith('#'):
+                    key, value = line.split('=', 1)
+                    key = key.strip()
+                    value = value.strip()
+
+                    # Remove quotes if present
+                    if (value.startswith("'") and value.endswith("'")) or \
+                       (value.startswith('"') and value.endswith('"')):
+                        value = value[1:-1]
+
+                    # Expand environment variables in the value
+                    value = os.path.expandvars(value)
+
+                    os.environ[key] = value
+
+
+# Load environment variables
+load_env()
+
+# Run the main script
 subprocess.run([sys.executable, os.path.join('src', 'main.py')])
