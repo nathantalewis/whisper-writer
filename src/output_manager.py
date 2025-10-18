d5df9cbd9e654a6bfa711744534c38c19c57815b
Nathan T.A. Lewis <nathan@nathantalewis.com>
2025-10-17 20:19:41 -0600

    Move the fcntl import to the uinput backend
    fcntl is a unix only module, so it shouldn't be imported when not needed.

--- src/output_manager.py
+++ src/output_manager.py
@@ -2,7 +2,6 @@ import subprocess
 import os
 import signal
 import time
-import fcntl
 import struct
 from pynput.keyboard import Key, Controller as PynputController
 
@@ -164,6 +163,8 @@ class UinputBackend:
         self.initialize_device()
 
     def initialize_device(self):
+        import fcntl
+        
         self.uinput_fd = os.open('/dev/uinput', os.O_WRONLY | os.O_NONBLOCK)
 
         # Enable key and synchronization events
@@ -242,5 +243,6 @@ class UinputBackend:
 
     def cleanup(self):
         if self.uinput_fd:
+            import fcntl
             fcntl.ioctl(self.uinput_fd, self.UI_DEV_DESTROY)
             os.close(self.uinput_fd)
